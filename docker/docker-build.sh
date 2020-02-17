1cb6246c6b0ced55f6fc1df081c063bea6914296
Manuel Alejandro Diaz Perez <manueldiazp92@gmail.com>
2020-02-17 11:18:36 -0500

    testing

--- docker/docker-build.sh
+++ docker/docker-build.sh
@@ -1,4 +1,4 @@
 export DOCKER_GIT_CREDENTIALS="$(cat ~/.git-credentials)"
-docker build -t mdiazp/appointments-test-service \
+docker build -t mdiazp/appointments-test-service:v2.0.0-alpha.0 \
 --build-arg DOCKER_GIT_CREDENTIALS \
 -f dockerfile ../app/.
